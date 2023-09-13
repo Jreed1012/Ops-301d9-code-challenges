5b906bf93a3f2732ed3dba4f2a4661a9b4471c8f
jermain reed <jreedsentry@gmail.com>
2023-09-13 04:22:45 +0000

    12

---
+++ Opchallenge12.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Script:                       Op Challenge 12
+# Author:                       Jermain 
+# Date of latest revision:      09/12/2023
+# Purpose:         
+
+destination_url= input ("Enter your desired URL:")
+
+#Prompt the user to select a HTTP Method of the following options
+http_methods = {
+1: "GET",
+2: "POST",
+3: "PUT",
+4: "DELETE",
+5: "HEAD",
+6: "PATCH",
+7: "OPTIONS"
+
+
+
+
+
+}
